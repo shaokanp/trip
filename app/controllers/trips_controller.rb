7ac199519634d24481c85e297b3b463e16de1284
shaokanp <whobirdsyou@gmail.com>
2014-02-01 01:01:04 +0800

    Trip permission * public trip can always be access * private trip can only be access by the owner

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,7 @@
 class TripsController < ApplicationController
-  before_action :signed_in_user, except:[:show]
-  before_action :correct_user, only:[:destroy]
+  before_action :check_user_permission, only:[:show]
+  before_action :signed_in_user
+  before_action :correct_user, only:[:update, :destroy]
 
   api :POST, '/trips', 'Create a new trip.'
   param :trip, Hash, required: true, desc: 'The trip to create.' do
@@ -49,6 +50,14 @@ class TripsController < ApplicationController
   def correct_user
     @trip = current_user.trips.find_by(id: params[:id])
     if @trip.nil?
+      flash[:error] = "Sorry, you don't have the permission to do that."
+      head :forbidden
+    end
+  end
+
+  def check_user_permission
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.user_id != current_user.id && !@trip.is_public
       flash[:error] = "Sorry, you don't have the permission to do that."
       head :forbidden
     end
