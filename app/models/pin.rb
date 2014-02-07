60f92ba4f03dd45b58fd2e2ae23ff3b5ac3a3ff2
shaokanp <whobirdsyou@gmail.com>
2014-02-08 02:18:59 +0800

    * Left column ui fixed * Add pin button function fixed

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  PIN_TYPE = [PIN_ATTRACTION = 'attraction', PIN_MEETING = 'meeting', PIN_ACCOMMODATION = 'accommodation', PIN_DINING = 'dining', PIN_TRANSPORT = 'transport']
+  PIN_TYPE = [PIN_ATTRACTION = 'attraction', PIN_MEETING = 'meeting', PIN_ACCOMMODATION = 'accommodation', PIN_DINING = 'dining', PIN_TRANSPORT = 'transport' ,PIN_UNDEFINED = 'undefined']
   belongs_to :trip
   has_many :notes
   validates :trip_id, presence: true
